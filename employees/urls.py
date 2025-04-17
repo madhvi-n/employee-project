fc3cb76151b6eaa7ea162111cee12dbcd93df8bd
madhvi-n <madhvinandanwar@gmail.com>
2025-04-17 14:32:54 +0530

    feat: added project assigment viewset

--- employees/urls.py
+++ employees/urls.py
@@ -11,10 +11,11 @@ router.register(r'projects', ProjectViewSet)
 employee_router = routers.NestedSimpleRouter(
     router, r'employees', lookup='employee'
 )
-employee_router.register(r'attendance', AttendanceViewSet)
-employee_router.register(r'performance', PerformanceRecordViewSet)
+employee_router.register(r'attendance', AttendanceViewSet, basename='employee-attendance')
+employee_router.register(r'performance', PerformanceRecordViewSet, basename='employee-performance')
 
 
 urlpatterns = [
     path("api/v1/", include(router.urls)),
+    path("api/v1/", include(employee_router.urls)),
 ]
--- employees/views.py
+++ employees/views.py
@@ -3,21 +3,19 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 
-from .models import Department, Employee, PerformanceRecord, Attendance, Project
+from .models import Department, Employee, PerformanceRecord, Attendance, Project, ProjectAssignment
 from .serializers import (
     DepartmentSerializer,
     EmployeeSerializer,
     PerformanceRecordSerializer,
     AttendanceSerializer,
     ProjectSerializer,
+    ProjectAssignmentSerializer
 )
-
 
 class CustomPagination(PageNumberPagination):
     page_size = 50
-    page_size_query_param = (
-        "page_size"  # Allow client to specify the number of items per page
-    )
+    page_size_query_param = "page_size"
     max_page_size = 100
 
 
@@ -27,6 +25,7 @@ class EmployeeViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["department__name", "role"]
     search_fields = ["name", "email"]
+    ordering_fields = ["name", "hire_date"]
     pagination_class = CustomPagination
 
 
@@ -50,4 +49,10 @@ class AttendanceViewSet(ModelViewSet):
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = CustomPagination
+
+
+class ProjectAssignmentViewSet(ModelViewSet):
+    queryset = ProjectAssignment.objects.all()
+    serializer_class = ProjectAssignmentSerializer
     pagination_class = CustomPagination
