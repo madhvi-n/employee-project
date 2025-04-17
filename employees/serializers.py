a6de95effcd4311fc7f51167b9abad1c68015923
madhvi-n <madhvinandanwar@gmail.com>
2025-04-17 13:38:05 +0530

    feat: added serializers

--- employees/serializers.py
+++ employees/serializers.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
-from employees.models import Employee
+from employees.models import Department, Employee, PerformanceRecord, Attendance, Project
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = ['id', 'name', 'email', 'age', 'department', 'role']
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ['id', 'name']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer()
+
+    class Meta:
+        model = Employee
+        fields = ['id', 'name', 'email', 'age', 'department', 'hire_date', 'role']
+
+class PerformanceRecordSerializer(serializers.ModelSerializer):
+    employee = EmployeeSerializer()
+
+    class Meta:
+        model = PerformanceRecord
+        fields = ['id', 'employee', 'rating', 'review_date', 'feedback']
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    employee = EmployeeSerializer()
+
+    class Meta:
+        model = Attendance
+        fields = ['id', 'employee', 'date', 'status']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    employee = EmployeeSerializer(many=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'name', 'employee']
