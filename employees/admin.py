d7303ab50b22ef64bc90938c882dbf18d797ad00
madhvi-n <madhvinandanwar@gmail.com>
2025-04-17 14:08:09 +0530

    fix: admin code cleanup

--- employees/admin.py
+++ employees/admin.py
@@ -1,46 +1,50 @@
 from django.contrib import admin
-from .models import Department, Employee, PerformanceRecord, Attendance, Project
+from .models import (
+    Department,
+    Employee,
+    PerformanceRecord,
+    Attendance,
+    Project,
+    ProjectAssignment,
+)
 
 
+@admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
 
 
-admin.site.register(Department, DepartmentAdmin)
-
-
+@admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email", "department", "hire_date", "role")
     list_filter = ("department", "role")
     search_fields = ("name", "email")
 
 
-admin.site.register(Employee, EmployeeAdmin)
-
-
+@admin.register(PerformanceRecord)
 class PerformanceRecordAdmin(admin.ModelAdmin):
     list_display = ("id", "employee", "rating", "review_date", "feedback")
     list_filter = ("employee", "rating")
     search_fields = ("employee__name",)
 
 
-admin.site.register(PerformanceRecord, PerformanceRecordAdmin)
-
-
+@admin.register(Attendance)
 class AttendanceAdmin(admin.ModelAdmin):
     list_display = ("id", "employee", "date", "status")
     list_filter = ("status", "employee")
     search_fields = ("employee__name",)
 
 
-admin.site.register(Attendance, AttendanceAdmin)
-
-
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     filter_horizontal = ("employee",)
     search_fields = ("name",)
 
 
-admin.site.register(Project, ProjectAdmin)
+@admin.register(ProjectAssignment)
+class ProjectAssignmentAdmin(admin.ModelAdmin):
+    list_display = ("employee", "project", "status", "assigned_date", "end_date")
+    list_filter = ("status", "assigned_date", "end_date")
+    search_fields = ("employee__name", "project__name")
